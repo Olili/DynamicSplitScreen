7df7e1ceea4871a5d6abc7466123e533d23ce306
Unknown <olivier.serris@wanadoo.fr>
2018-01-29 17:42:04 +0100

    Remove Unused protoCode

--- Assets/Script/MeshHelper.cs
+++ Assets/Script/MeshHelper.cs
@@ -49,25 +49,20 @@ namespace VoronoiSplitScreen
         */
         public static Mesh GetPolygon(int site, List<GraphEdge> listEdges, Vector3[] sitePosList)
         {
-            //        // get Vertices around a site
-            //List<Vector3> sitePolyVerticesList = ComputeSiteVertices(site,listEdges, sitePosList);
-            //        // sort vertices around Site
-            //Vector3[] sortedvertices = SortSiteVertices(sitePolyVerticesList);
-
             Vector3[] sortedvertices = GetPolyVertices(site, sitePosList, listEdges);
 
-                    // Convert Vertices in 2d
+            // Convert Vertices in 2d
             Vector2[] sitePolyVertices2D = new Vector2[sortedvertices.Length];
             for (int i = 0; i < sitePolyVertices2D.Length; i++)
                 sitePolyVertices2D[i] = (new Vector2(sortedvertices[i].x, sortedvertices[i].y));
-                    // build mesh  with Vertices in 2d
+            // build mesh  with Vertices in 2d
             Triangulator tr = new Triangulator(sitePolyVertices2D);
             int[] indices = tr.Triangulate();
 
             Vector3[] vertices = new Vector3[sitePolyVertices2D.Length];
             for (int i = 0; i < vertices.Length; i++)
                 vertices[i] = (new Vector3((sitePolyVertices2D[i].x - Screen.width / 2) / Screen.width,
-                                            (Screen.height-sitePolyVertices2D[i].y - Screen.height / 2) / Screen.height))*2;
+                                            (Screen.height - sitePolyVertices2D[i].y - Screen.height / 2) / Screen.height)) * 2;
 
             Mesh polygone = new Mesh();
             polygone.vertices = vertices;
@@ -84,9 +79,9 @@ namespace VoronoiSplitScreen
                 -->E02 Si non je cherche parmis les bords qui lui appartiennt un bord qui est lié à ce truc.
             --> stop lorsque je retourne a la première arrivée.
         */
-        static Vector3[] GetPolyVertices(int curSite, Vector3[] sitePosList,List<GraphEdge> listEdges)
+        static Vector3[] GetPolyVertices(int curSite, Vector3[] sitePosList, List<GraphEdge> listEdges)
         {
-                // creation des borderVertices
+            // creation des borderVertices
             List<Vector3> borderVertices = new List<Vector3>
             {
                 new Vector3(0, 0, 0),
@@ -120,8 +115,8 @@ namespace VoronoiSplitScreen
                 if (listEdges[i].x1 == listEdges[i].x2 && listEdges[i].y1 == listEdges[i].y2)
                     continue;
                 if ((listEdges[i].site1 == curSite || listEdges[i].site2 == curSite) &&
-                    templistEdges.Find(edge =>  edge.x1 == listEdges[i].x1 && edge.x2 == listEdges[i].x2
-                                        &&      edge.y1 == listEdges[i].y1 && edge.y2 == listEdges[i].y2) == null)
+                    templistEdges.Find(edge => edge.x1 == listEdges[i].x1 && edge.x2 == listEdges[i].x2
+                                        && edge.y1 == listEdges[i].y1 && edge.y2 == listEdges[i].y2) == null)
                     templistEdges.Add(listEdges[i]);
             }
 
@@ -177,7 +172,7 @@ namespace VoronoiSplitScreen
                     {
                         sortedvertices.Add(curPoint);
                         curID++;
-                        if (curID>10)
+                        if (curID > 10)
                         {
                             Debug.LogError("To many curSite : " + curSite);
                             break;
@@ -188,106 +183,13 @@ namespace VoronoiSplitScreen
             while (curPoint != firstPoint);
 
             Vector3[] sortedVerticeTab = new Vector3[sortedvertices.Count];
-            for (int i = 0; i < sortedvertices.Count;i++)
+            for (int i = 0; i < sortedvertices.Count; i++)
             {
                 sortedVerticeTab[i] = sortedvertices[i];
             }
             return sortedVerticeTab;
         }
 
-        static List<Vector3> ComputeSiteVertices(int site, List<GraphEdge> listEdges, Vector3[] sitePosList )
-        {
-            List<Vector3> borderVertices = new List<Vector3>
-            {
-                new Vector3(0, 0, 0),
-                new Vector3(Screen.width,0 , 0),
-                new Vector3(0, Screen.height, 0),
-                new Vector3(Screen.width, Screen.height, 0),
-            };
-            List<Vector3> sitePolyVertices = new List<Vector3>();
-                // On regroupe les vertex en fonction de leur sites : 
-            AddGraphVerticesToSiteVertices(site, listEdges, sitePolyVertices);
-            AddBorderVerticeToSiteVertices(site, borderVertices, sitePosList, sitePolyVertices);
-
-            //Debug.Log("site : " + site);
-            //for (int  i = 0;i < sitePolyVertices.Count;i++)
-            //{
-            //    Debug.Log(sitePolyVertices[i]);
-            //}
-            return sitePolyVertices;
-        }
-
-        static Vector3[] SortSiteVertices(List<Vector3> polyVertices)
-        {
-            // je pose les vertex déjà liés. 
-            // j'ajoute les vertex sur une meme ligne.
-            Vector3[] sortedPolyVertices = new Vector3[polyVertices.Count];
-
-
-            // je commence sur un vertex arbitraire
-            // je récupère le plus proche. 
-            // Je récupère son plus proche différent des autres et ainsi de suite 
-            for (int i = 0; i < polyVertices.Count; i++)
-                sortedPolyVertices[i] = polyVertices[i];
-            return sortedPolyVertices;
-        }
-
-        static void AddGraphVerticesToSiteVertices(int curSite, List<GraphEdge> listEdges,List<Vector3> siteVertices)
-        {
-            Debug.Assert(siteVertices != null, "list must not be null");
-            for (int i = 0; i < listEdges.Count; i++)
-            {
-                if (listEdges[i].site1 == curSite || listEdges[i].site2 == curSite)
-                {
-                    Vector3 vertex = new Vector3((float)listEdges[i].x1, (float)listEdges[i].y1, 0);
-                    if (!siteVertices.Contains(vertex))
-                        siteVertices.Add(vertex);
-                    vertex = new Vector3((float)listEdges[i].x2, (float)listEdges[i].y2, 0);
-                    if (!siteVertices.Contains(vertex))
-                        siteVertices.Add(vertex);
-                }
-            }
-        }
-        static void AddBorderVerticeToSiteVertices(int curSite, List<Vector3> borderVertices, Vector3[] sitePosList, List<Vector3> siteVertices)
-        {
-            Debug.Assert(siteVertices != null, "list must not be null");
-            List<Vector3> tempBorderVertices =new List<Vector3>();
-            for (int i = 0; i < borderVertices.Count; i++)
-            {
-                float shortestDistance = float.MaxValue;
-                float shortestSiteId = 0;
-                for (int j = 0; j < sitePosList.Length;j++)
-                {
-                    float distance = Vector3.Distance(sitePosList[j], borderVertices[i]);
-                    if (distance < shortestDistance)
-                    {
-                        shortestSiteId = j;
-                        shortestDistance = distance;
-                    }
-                }
-                if (shortestSiteId == curSite)
-                {
-                    //siteVertices.Add(borderVertices[i]);
-                    tempBorderVertices.Add(borderVertices[i]);
-                }
-            }
-            int nbGraph = siteVertices.Count;
-            int nbBorderAdded = 0;
-            for (int i = 0; nbBorderAdded < tempBorderVertices.Count; i = (i + 1) % tempBorderVertices.Count) 
-            {
-                if ((siteVertices[nbGraph + nbBorderAdded - 1].x == tempBorderVertices[i].x ||
-                    siteVertices[nbGraph + nbBorderAdded - 1].y == tempBorderVertices[i].y) &&
-                    !siteVertices.Contains(tempBorderVertices[i]))
-                {
-                    nbBorderAdded++;
-                    siteVertices.Add(tempBorderVertices[i]);
-                }
-            }
-        }
-        public static void CreatePolygoneFromVertices(Vector3 vertices)
-        {
-        }
     }
-
 }
 
